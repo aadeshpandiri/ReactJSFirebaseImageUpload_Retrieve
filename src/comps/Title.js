c0cb7a6f25eed3e3e734400714fdfdd162cd053a
aadeshpandiri <aadeshpandiri@gmail.com>
2021-03-29 20:53:21 +0530

    my first commit

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>SaFE WaLleT</h1>
+      <h2>Your Pictures</h2>
+      <p><b>Hey There! Are you secured? Dont Worry We are :) </b></p>
+    </div>
+  )
+}
+
+export default Title;
---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyA92qZwFM_fS4LQ7Q1YNC7Gcni5wxlUko4",
+  authDomain: "login-9ac5f.firebaseapp.com",
+  projectId: "login-9ac5f",
+  storageBucket: "login-9ac5f.appspot.com",
+  messagingSenderId: "536911529404",
+  appId: "1:536911529404:web:d30d1c8cd5b9e55b8ed7b9",
+  measurementId: "G-256KB8VTTQ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectStorage, projectFirestore, timestamp };
